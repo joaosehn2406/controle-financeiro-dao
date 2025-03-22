79e7e0ed8780d2333b3be0acabc2cabd69d1c686
Joao Pedro Sehnem dos Santos <jpssantosos5874@gmail.com>
2025-03-22 10:58:39 -0300

    Class Usuario created

---
+++ src/model/Usuario.java
@@ -0,0 +1,67 @@
+package model;
+
+import java.io.Serializable;
+
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private int id;
+    private String nome;
+    private String email;
+    private String senha;
+
+
+    public Usuario() {
+    }
+
+
+    public Usuario(int id, String nome, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenhaHash(String senha) {
+        this.senha = senha;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
