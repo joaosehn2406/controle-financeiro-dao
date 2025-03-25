6243b35f7b84ea9cda3a66177f7a513e3c9aa849
joaosehn2406 <jpssantosos5874@gmail.com>
2025-03-25 15:54:45 -0300

    MovimentacaoService refactor

--- src/services/MovimentacaoService.java
+++ src/services/MovimentacaoService.java
@@ -10,34 +10,62 @@ public class MovimentacaoService {
     private MovimentacaoDao movimentacaoDao;
     private MovimentacaoAuth movimentacaoAuth;
 
+
     public MovimentacaoService(MovimentacaoDao movimentacaoDao, MovimentacaoAuth movimentacaoAuth) {
         this.movimentacaoDao = movimentacaoDao;
-        this.movimentacaoAuth = new MovimentacaoAuth(movimentacaoDao);
+        this.movimentacaoAuth = movimentacaoAuth; 
     }
 
 
     public void adicionarMovimentacao(Movimentacao movimentacao) {
-        if (movimentacao.getValor() <= 0) {
-            throw new MovimentacaoException("O valor da movimentação deve ser maior que zero.");
+
+        movimentacaoAuth.validarValorMovimentacao(movimentacao); 
+        if (!movimentacaoAuth.validarCategoria(movimentacao)) {
+            throw new MovimentacaoException("Categoria inválida.");
         }
+        if (!movimentacaoAuth.validarUsuario(movimentacao)) {
+            throw new MovimentacaoException("Usuário inválido.");
+        }
+        movimentacaoAuth.validarDataMovimentacao(movimentacao); 
+        if (movimentacaoAuth.verificarDuplicidade(movimentacao)) {
+            throw new MovimentacaoException("Movimentação já existe.");
+        }
+
+
         movimentacaoDao.insert(movimentacao);
     }
 
+
     public void removerMovimentacao(Integer id) {
-        
+   
         if (movimentacaoAuth.verificarExistenciaMovimentacao(id)) {
-            movimentacaoDao.deleteByMovimentacaoId(id);
+            movimentacaoDao.deleteByMovimentacaoId(id); 
         } else {
             throw new MovimentacaoException("Não há essa movimentação.");
         }
     }
 
+
     public void atualizarMovimentacao(Movimentacao movimentacao) {
         if (movimentacao.getId_transacao() <= 0) {
-            throw new MovimentacaoException("Movimentação inexistente");
+            throw new MovimentacaoException("Movimentação inexistente.");
         }
+
+        movimentacaoAuth.validarValorMovimentacao(movimentacao); 
+        if (!movimentacaoAuth.validarCategoria(movimentacao)) {
+            throw new MovimentacaoException("Categoria inválida.");
+        }
+        if (!movimentacaoAuth.validarUsuario(movimentacao)) {
+            throw new MovimentacaoException("Usuário inválido.");
+        }
+        movimentacaoAuth.validarDataMovimentacao(movimentacao);
+        if (movimentacaoAuth.verificarDuplicidade(movimentacao)) {
+            throw new MovimentacaoException("Movimentação já existe.");
+        }
+
         movimentacaoDao.update(movimentacao);
     }
+
 
     public Movimentacao buscarMovimentacaoPorId(int idTransacao) {
         Movimentacao movimentacao = movimentacaoDao.findByMovimentacaoId(idTransacao);
