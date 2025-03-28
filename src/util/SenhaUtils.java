7f2f0cf14efa64641bca55bfad7567626a08864e
joaosehn2406 <jpssantosos5874@gmail.com>
2025-03-28 09:47:36 -0300

    Removed unecessary lib

---
+++ src/util/SenhaUtils.java
@@ -0,0 +1,19 @@
+package util;
+
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+
+public class SenhaUtils {
+
+    private static final Argon2 argon2 = Argon2Factory.create();
+
+
+    public static String gerarHash(String senha) {
+        return argon2.hash(2, 65536, 1, senha);
+    }
+
+
+    public static boolean verificarSenha(String senha, String hash) {
+        return argon2.verify(hash, senha);
+    }
+}
